6a3e703d435520a5795e4f57b553b03305a4854c
amandragon <aee8@duke.edu>
2014-03-10 15:36:25 -0700

    search box is working

--- app/books_controller.js
+++ app/books_controller.js
@@ -6,6 +6,12 @@ bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
 
+   $scope.addToCart = function(book, cart) {
+
+
+
+
+    };
   //Code to manage cart goes here
 
 };
