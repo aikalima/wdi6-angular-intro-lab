cc692b1724cdaab815d1b7336cf2baaa41586209
Eric Perez <ericricardoperez@gmail.com>
2014-03-10 15:57:19 -0700

    order books by select box value

--- app/books_controller.js
+++ app/books_controller.js
@@ -6,6 +6,9 @@ bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
 
+  $scope.selects = ['author', 'title', 'price']
+  $scope.select = 'title'
+
   //Code to manage cart goes here
 
 };
