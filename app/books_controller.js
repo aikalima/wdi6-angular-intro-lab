d9d4501c7b3d29b2ac1debd840b04f27b2df2674
amandragon <aee8@duke.edu>
2014-03-10 16:14:33 -0700

    remove functionality working sort of

--- app/books_controller.js
+++ app/books_controller.js
@@ -6,12 +6,24 @@ bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
 
-   $scope.addToCart = function(book, cart) {
-    // add book's title, author, and price
+   $scope.addToCart = function(book) {
+    $scope.books.push(book);
+    $scope.book = {};
+    }
 
-
-
-    };
   //Code to manage cart goes here
+//    $scope.totalPrice = function(){
+//         var total = 0;
+//  for(count=0;count<$scope.items.length;count++){
+//      total += $scope.books[count].Price*$scope.books[count].Quantity;
+//  }
+//  return total;
+//     }
+   
+    $scope.removeBook = function(index){
+        $scope.books.splice(index,1);
+    }
+// }
+
 
 };
