3780d7219e331feb338d5e025c3903685889c370
jongmini <jongmin.choi@gmail.com>
2014-03-10 16:23:04 -0700

    can search by title. no drop down. can add items to the cart

--- app/books_controller.js
+++ app/books_controller.js
@@ -6,6 +6,27 @@ bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
 
+  $scope.queryBy = function(selection){
+    console.log(selection);
+  };
+
+  $scope.add = function(index){
+    console.log(index);
+    $scope.items.push(books[index]);
+    // $scope.priceList.push(books[index].price);
+    
+  };
+
+  $scope.total = function(){
+    var sum = 0;
+    for(var i=0; i<$scope.item.length; i++){
+      sum += $scope.item[i].price;
+    }
+    return sum;
+  };
+
+  $scope.items = [];
   //Code to manage cart goes here
+
 
 };
