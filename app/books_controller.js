ab661e74f7090afd0c75dd2f2b4fabe58e2b2e35
Calvin Lam <calvinwlam@gmail.com>
2014-03-10 23:15:48 -0700

    dropdown sort

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,6 +5,7 @@ bookly.bookApp = angular.module("bookApp", []);
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
+  $scope.orderProp = 'title';
 
   //Code to manage cart goes here
 
