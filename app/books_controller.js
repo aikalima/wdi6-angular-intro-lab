af5df5761339ca7e6732dcabee78279ea11807f0
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-03-11 09:53:56 -0700

    got everything to work

--- app/books_controller.js
+++ app/books_controller.js
@@ -12,19 +12,45 @@ bookly.BooksController = function($scope) {
   // { id:3, sort: "Price", sortbyPrice: books.price},
   // ];
   $scope.shoppingCart = [];
+
   $scope.addShop = function(item){
-     $scope.shoppingCart.push(item);
+    if (arrayContains(this.shoppingCart, item)){
+      $scope.shoppingCartItems += 1;
+      var update = this.shoppingCart.indexOf(item);
+      $scope.shoppingCart[update].quantity += 1;
+    } else {
+      $scope.shoppingCartItems += 1;
+      this.shoppingCart.push(item);
+      var newItem = this.shoppingCart.indexOf(item);
+      $scope.shoppingCart[newItem].quantity = 1;
+      console.log(this.shoppingCart); 
+    }
+      
   };
+
+  $scope.shoppingCartItems = 0;
+
+  function arrayContains(arr,element){
+    if(arr !== null && arr.length >0){
+        for(var i=0;i<arr.length;i++){
+            if(arr[i] == element)
+                return true;
+        }
+    }
+    return false;
+ } 
 
   $scope.total = function(){
     var total = 0;
     $scope.shoppingCart.forEach(function(x){
-        total += x.price;
+      var amount = x.quantity * x.price;
+        total += amount;
     });
     return total;
   };
 
   $scope.emptyCart = function(cart){
+     $scope.shoppingCartItems = 0;
      $scope.shoppingCart.splice($scope.toRemove);
   };
 
