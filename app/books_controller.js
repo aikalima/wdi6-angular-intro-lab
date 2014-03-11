f0809efb41efda01d74be8c4d7ebe3831e161c74
Ryan Brandt <Ryabrandt@gmail.com>
2014-03-10 17:59:56 -0700

    most buttons working, duplicates still show

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,7 +5,22 @@ bookly.bookApp = angular.module("bookApp", []);
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
+  $scope.checkout = [];
+  $scope.itemCount = 0;
 
   //Code to manage cart goes here
+  $scope.add = function(index){
+    if($scope.checkout[$scope.books[index]]){
+      console.log('found');
+    } else { 
+    $scope.checkout.push($scope.books[index]);
+    $scope.itemCount++;
+    }
+  };
+
+  $scope.empty = function(){
+    $scope.checkout = [];
+    $scope.itemCount = 0;
+  };
 
 };
