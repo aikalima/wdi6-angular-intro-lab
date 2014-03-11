eb252351894cbcc40f5f3843d8216a94d4b679ea
amandragon <aee8@duke.edu>
2014-03-10 23:12:33 -0700

    itemCount function working!

--- app/books_controller.js
+++ app/books_controller.js
@@ -10,7 +10,7 @@ bookly.BooksController = function($scope) {
 
    $scope.addToCart = function(book) {
     $scope.cart.push(book);
-    itemCount + 1;
+    $scope.itemCount ++;
   };
     
    $scope.total = function(){
