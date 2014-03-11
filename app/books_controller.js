9d10fb61fcf4055c2f35368a3bc12761619299e8
David Simons <dave.c.simons@gmail.com>
2014-03-10 17:05:19 -0700

    functioning add to cart and remove items from cart

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,10 +5,30 @@ bookly.bookApp = angular.module("bookApp", []);
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
-
+  
   $scope.selects = ['author', 'title', 'price'];
   $scope.select = 'title';
+  $scope.cartBooks = [];
+  $scope.orderTotal = 0;
+  $scope.cartItemCount = $scope.cartBooks.length;
+
+
 
   //Code to manage cart goes here
+	$scope.addToCart = function (id) {
+		var book = _.find(books, function(b) {
+			return b.id === id;
+		});
+		$scope.cartBooks.push(book);
+		$scope.orderTotal += book.price;
+		$scope.cartItemCount = $scope.cartBooks.length;
+
+	};
+
+	$scope.emptyCart = function() {
+		$scope.cartBooks = [];
+		$scope.cartItemCount = $scope.cartBooks.length;
+		$scope.orderTotal = 0;
+	};
 
 };
