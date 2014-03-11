85640a48f50f5f5b07f46d47ec4e45e6983d5359
smspezzano <smspezzano@gmail.com>
2014-03-10 19:10:17 -0700

    completed basic requests

--- app/books_controller.js
+++ app/books_controller.js
@@ -7,5 +7,31 @@ bookly.BooksController = function($scope) {
   $scope.books = books;
 
   //Code to manage cart goes here
+  $scope.sortOptions = {
+    selections: [
+      {id : 1, name : 'Author' },      
+      {id : 2, name : 'Title' },
+      {id : 3, name : 'Price' },
+    ]
+  };
 
+  $scope.cartItems = [];
+  $scope.total = 0;
+  $scope.cartCount = 0;
+  var total = 0;
+
+  $scope.add = function(index){
+    if ($scope.cartItems.indexOf($scope.books[index]) === -1){
+      $scope.cartItems.push($scope.books[index]);
+      total += $scope.books[index].price;
+      $scope.total = total.toFixed(2);
+    }
+    $scope.cartCount = $scope.cartItems.length;
+  };
+
+  $scope.empty = function(){
+    $scope.cartItems.length = 0;
+    $scope.total = 0;
+    $scope.cartCount = 0;
+  };
 };
