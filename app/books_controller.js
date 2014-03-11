39b0a20809adadb51fadb6f32e83cecab595a838
melodicodyssey <stevedbauer@gmail.com>
2014-03-10 17:58:03 -0700

    books adding to cart

--- app/books_controller.js
+++ app/books_controller.js
@@ -7,6 +7,14 @@ bookly.BooksController = function($scope) {
   $scope.books = books;
 
   //Code to manage cart goes here
-  
+  $scope.cart = [];
 
+  $scope.pushToCart = function(index){
+    console.log($scope.books[index]);
+    console.log($scope.cart.indexOf($scope.books[index]));
+    console.log($scope.cart);
+    // if (!$scope.cart.indexOf($scope.books[index]) > 0) {
+      $scope.cart.push($scope.books[index]);
+    // }
+  };
 };
