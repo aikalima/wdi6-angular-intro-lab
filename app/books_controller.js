2bc942da51908ca0e7c7f0adf1a1ce2e7811cd2c
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-03-10 18:12:27 -0700

    got basic requirements

--- app/books_controller.js
+++ app/books_controller.js
@@ -6,6 +6,29 @@ bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
 
+  // $scope.categories = [
+  // { id:1, sort: "Title", sortbyTitle: books.title},
+  // { id:2, sort: "Author", sortbyAuthor: books.author},
+  // { id:3, sort: "Price", sortbyPrice: books.price},
+  // ];
+  $scope.shoppingCart = [];
+  $scope.addShop = function(item){
+     $scope.shoppingCart.push(item);
+  };
+
+  $scope.total = function(){
+    var total = 0;
+    $scope.shoppingCart.forEach(function(x){
+        total += x.price;
+    });
+    return total;
+  };
+
+  $scope.emptyCart = function(cart){
+     $scope.shoppingCart.splice($scope.toRemove);
+  };
+
   //Code to manage cart goes here
 
 };
+
