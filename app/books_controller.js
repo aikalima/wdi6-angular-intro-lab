79559f2933454f8397df513a6758dd1f86adc880
dvmonroe <dvmonroe6@gmail.com>
2014-03-10 22:22:58 -0700

    search works and add item to cart working

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,14 +5,62 @@ bookly.bookApp = angular.module("bookApp", []);
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
+  $scope.cart = {
+        books: [{
+            qty: '1',
+            title: '',
+            author: '',
+            price: ''}]
+    };
+  $scope.count = 0;
+  $scope.total = 0;
 
-  $scope.options = [
-    { label: 'title', },
-    { label: 'author', },
-    { label: 'price', }
-  ];
 
-  //Code to manage cart goes here
+  //add a book to the shopping cart
+  $scope.addItemToCart = function(book) {
+    $scope.cart.books.push(book);
+    };
+
+  //total number of items in the cart
+  $scope.cartCount = function() {
+    var count = 0;
+    angular.forEach($scope.cart.books, function(book) {
+    count += book;
+    });
+    return count;
+  };
+
+
+  //clear the entire cart
+  $scope.clearCart = function() {
+    $scope.cart.books = [];
+  };
+
+  //total price of all items in the cart
+  $scope.totalPrice = function(){
+    var total = 0;
+    for(count=0; count < $scope.cart.length; count++){
+    total += $scope.cart.books.price;
+    }
+ return total;
+    };
+
 
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
