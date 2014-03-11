2d4fa5a4b85909f3acf151a2132f9f18e5000050
kaifius <kai.hofius@gmail.com>
2014-03-10 17:38:46 -0700

    add books and sort done

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,7 +5,10 @@ bookly.bookApp = angular.module("bookApp", []);
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
-
+  $scope.cart = [];
+  $scope.addToCart = function(book){
+    $scope.cart.push(book);
+  }
   //Code to manage cart goes here
 
 };
