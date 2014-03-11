d5f9259d9ce00573446045aa0231ad936ce4c42e
mayormcmatt <matthew.is.riding@gmail.com>
2014-03-10 17:04:38 -0700

    sort working

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,7 +5,5 @@ bookly.bookApp = angular.module("bookApp", []);
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
-
-  //Code to manage cart goes here
-
+  $scope.ordering = 'title';
 };
