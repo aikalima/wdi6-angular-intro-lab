1a6ed4b8cbfcdfadf66386a30b4a3f1eefd2e895
afleisch <afleisch@gmail.com>
2014-03-10 19:57:04 -0700

    cart total amount shows and updates

--- app/books_controller.js
+++ app/books_controller.js
@@ -6,12 +6,13 @@ bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
   $scope.cartItems = [];
-  
+  $scope.total = 0
 
   $scope.add = function(index) {
     $scope.book = $scope.books[index]
     $scope.cartItems.push($scope.book)
-  }
- 
-
+    $scope.total += $scope.book.price
+     
+  };
 };
+
