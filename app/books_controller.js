5a86679eaf59eb6a475177333182a8ae1f3a87d9
amandragon <aee8@duke.edu>
2014-03-10 22:38:06 -0700

    add method on the way

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,11 +5,36 @@ bookly.bookApp = angular.module("bookApp", []);
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
+  $scope.cart = [];
+  $scope.itemCount = 0;
+
+
 
    $scope.addToCart = function(book) {
-    $scope.books.push(book);
-    $scope.book = {};
-    }
+    var addedBook = $scope.books[book];
+    if($scope.cart.indexOf(addedBook) >= 0){
+ $scope.cart[$scope.cart.indexOf(addedBook)].quantity += 1;
+ } else {
+  addedBook.quantity = 1;
+    $scope.cart.push(addedBook);
+  }
+  $scope.itemCount++;
+  };
+};
+    
+
+
+  //     $scope.add = function(index){
+  //   var addedBook = $scope.books[index];
+    
+  //   if($scope.checkout.indexOf(addedBook) >= 0){
+  //     $scope.checkout[$scope.checkout.indexOf(addedBook)].quantity += 1;
+  //   } else {
+  //     addedBook.quantity = 1;
+  //     $scope.checkout.push(addedBook);
+  //   }
+  //   $scope.itemCount++;
+  // };
 
   //Code to manage cart goes here
 //    $scope.totalPrice = function(){
@@ -20,10 +45,8 @@ bookly.BooksController = function($scope) {
 //  return total;
 //     }
    
-    $scope.removeBook = function(index){
-        $scope.books.splice(index,1);
-    }
-// }
+//     $scope.removeBook = function(index){
+//         cart_item === 0;
+//     }
+// // }
 
-
-};
