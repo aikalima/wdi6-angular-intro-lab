0b909ece7e98e914c26ef00f59f5b8ace456ec02
Calvin Lam <calvinwlam@gmail.com>
2014-03-11 00:29:56 -0700

    can add and remove item from cart

--- app/books_controller.js
+++ app/books_controller.js
@@ -1,12 +1,40 @@
 var bookly = bookly || {};
 
-bookly.bookApp = angular.module("bookApp", []);
+bookly.bookApp = angular.module("bookApp", [
+    //'ngRoute', 'bookly.BooksController']);
+
+    //bookly.bookApp.config(['$routeProvider',
+    //function
+]);
 
 bookly.BooksController = function($scope) {
-  // load books. They are defined in data.js (we've got no back end yet!)
-  $scope.books = books;
-  $scope.orderProp = 'title';
+    // load books. They are defined in data.js (we've got no back end yet!)
+    $scope.books = books;
+    $scope.orderProp = 'title';
+    $scope.cart = [];
+    $scope.itemCount = 0;
 
-  //Code to manage cart goes here
+    //Code to manage cart goes here
+    $scope.addBook = function(index) {
+        var orderBook = $scope.books[index];
+
+        $scope.cart.push(orderBook);
+        $scope.itemCount++;
+
+    };
+    $scope.emptyCart = function(index) {
+        $scope.cart = [];
+        $scope.itemCount = 0;
+     
+    };
+    $scope.orderTotal = function() {
+        for (var i = 0; i < $scope.cart.length; i++) {
+            tempPrice = 0;
+            tempPrice += $scope.cart[i].price;
+            i++;
+            console.log($scope.cart[i]);
+        }
+        return this;
+    };
 
 };
