fd434053ef440570261afb0b082be5c275215d63
yutaka_houlette <yutakahoulette@gmail.com>
2014-03-11 16:40:23 -0700

    first commit. already got search, dropdown, item count and table to work

--- app/books_controller.js
+++ app/books_controller.js
@@ -4,8 +4,18 @@ bookly.bookApp = angular.module("bookApp", []);
 
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
+  // $scope is like @ in ruby => instance variable
+  
   $scope.books = books;
 
-  //Code to manage cart goes here
+  var count = 0;
 
+  $scope.addToCart = function (aBook) {
+    $scope.count = count++;
+    $scope.author = aBook.author;
+    $scope.title = aBook.title;
+    $scope.price = aBook.price;
+  };
 };
+
+// modules are like large functions
