883a22b16e28e48687f6a1414b37d82cd50eb5ba
amandragon <aee8@duke.edu>
2014-03-10 22:43:09 -0700

    empty cart may be working

--- app/books_controller.js
+++ app/books_controller.js
@@ -11,7 +11,7 @@ bookly.BooksController = function($scope) {
 
 
    $scope.addToCart = function(book) {
-    var addedBook = $scope.books[book];
+    var addedBook = $scope.books[index];
     if($scope.cart.indexOf(addedBook) >= 0){
  $scope.cart[$scope.cart.indexOf(addedBook)].quantity += 1;
  } else {
@@ -22,31 +22,17 @@ bookly.BooksController = function($scope) {
   };
 };
     
-
-
-  //     $scope.add = function(index){
-  //   var addedBook = $scope.books[index];
-    
-  //   if($scope.checkout.indexOf(addedBook) >= 0){
-  //     $scope.checkout[$scope.checkout.indexOf(addedBook)].quantity += 1;
-  //   } else {
-  //     addedBook.quantity = 1;
-  //     $scope.checkout.push(addedBook);
-  //   }
-  //   $scope.itemCount++;
-  // };
-
-  //Code to manage cart goes here
-//    $scope.totalPrice = function(){
-//         var total = 0;
-//  for(count=0;count<$scope.items.length;count++){
-//      total += $scope.books[count].Price*$scope.books[count].Quantity;
-//  }
-//  return total;
-//     }
+   $scope.total = function(){
+        var total = 0;
+ for(count=0;count<$scope.books.length;count++){
+     total += $scope.books[count].Price*$scope.books[count].Quantity;
+ }
+ return total;
+    }
    
-//     $scope.removeBook = function(index){
-//         cart_item === 0;
-//     }
+    $scope.emptyCart = function(index){
+      $scope.cart === [];
+      $scope.itemCount === 0;
+    }
 // // }
 
