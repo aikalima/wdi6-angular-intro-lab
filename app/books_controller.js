c9735fed78dade68493ff1ba0c68bc3a20d62836
kaifius <kai.hofius@gmail.com>
2014-03-10 19:21:44 -0700

    changed addToCart some

--- app/books_controller.js
+++ app/books_controller.js
@@ -6,16 +6,24 @@ bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
   $scope.cart = [];
-  $scope.addToCart = function(book){
-    $scope.cart.push(book);
+
+  $scope.addToCart = function(book, cart){
+    if (_.findWhere(cart, {id: book.id})) {
+      book.qty = book.qty + 1;
+    }else{
+      book.qty = 1;
+      $scope.cart.push(book);
+    }
+
+
   };
 
   $scope.cartTotal = function(cart){
-    var total = 0;
+    var total = 0.00;
     _.each(cart, function(book){
       total = total + book.price;
     });
-    return total;
+    return total.toFixed(2);
   };
 
   $scope.emptyCart = function(cart){
@@ -24,6 +32,6 @@ bookly.BooksController = function($scope) {
 
   $scope.quantify = function(cart, book_id){
     return _.where(cart, {id: book_id}).length;
-  }
+  };
 
 };
