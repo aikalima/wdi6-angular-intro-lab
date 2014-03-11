828c42747326882098552b2c032515f2e915a797
mayormcmatt <matthew.is.riding@gmail.com>
2014-03-11 09:01:31 -0700

    all features added, issue with add to cart when sorted

--- app/books_controller.js
+++ app/books_controller.js
@@ -4,6 +4,29 @@ bookly.bookApp = angular.module("bookApp", []);
 
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
-  $scope.books = books;
-  $scope.ordering = 'title';
+    $scope.books = books;
+    $scope.ordering = 'title';
+    $scope.addItem = function(index){
+      console.log(index);
+    };
+
+    $scope.addItemToCart = function(index) {
+        var cart_item = {book: books[index]};
+        $scope.cart.push(cart_item);
+    };
+
+    $scope.cart = [];
+
+    $scope.emptyCart = function() {
+        var remove = $scope.cart.splice(0, $scope.cart.length);
+    };
+
+    $scope.updateTotal = function() {
+        var total = 0.0;
+        var length = $scope.cart.length;
+        for(var i = 0; i < length; i++) {
+            total += $scope.cart[i].book.price;
+        }
+        return total;
+    };
 };
