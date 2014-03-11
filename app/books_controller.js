5d202ea5ddddd93ff38cf3c8a1e0e62ff60662a8
jongmini <jongmin.choi@gmail.com>
2014-03-10 18:26:24 -0700

    can order by title author price

--- app/books_controller.js
+++ app/books_controller.js
@@ -6,29 +6,18 @@ bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
 
-  // $scope.queryBy = function(selection){
-  //   console.log(selection);
-  // };
   $scope.items = [];
   $scope.total = 0;
   $scope.totalItems = 0;
 
 
   $scope.add = function(index){
-    // // check if book is in cart
-    // // if so, find item in cart and increase count
-    // // elese create new cart_item with count 1 and push
-    // cart_item = {
-    //   book: books[index],
-    //   count: 1
-    // }
-    // $scope.items.push(cart_item);
 
     $scope.totalItems += 1;
     books[index].count = books[index].count || 0;
     if(books[index].count===0){
-      books[index].count = 1;
       $scope.items.push(books[index]);
+      books[index].count = 1;
     } else {
       books[index].count += 1;
     }
