d71868824bb749cc4f4768ab61448c5368d4dda7
melodicodyssey <stevedbauer@gmail.com>
2014-03-10 21:53:00 -0700

    counter, books counter, subtotal, Total functionality added to cart

--- app/books_controller.js
+++ app/books_controller.js
@@ -13,8 +13,36 @@ bookly.BooksController = function($scope) {
     console.log($scope.books[index]);
     console.log($scope.cart.indexOf($scope.books[index]));
     console.log($scope.cart);
-    // if (!$scope.cart.indexOf($scope.books[index]) > 0) {
+
+    if ($scope.cart.indexOf($scope.books[index]) >= 0) {
+      $scope.cart[$scope.cart.indexOf($scope.books[index])].count += 1;
+    } else {
       $scope.cart.push($scope.books[index]);
-    // }
+      $scope.cart[$scope.cart.indexOf($scope.books[index])].count = 1;
+    }
+  };
+
+  $scope.total = function() {
+    var total = 0.0;
+    for (var i = 0; i < $scope.cart.length; i++) {
+      total += ($scope.cart[i].price) * ($scope.cart[i].count);
+    }
+    return total;
+  };
+
+  $scope.quantity = function() {
+    var quantity = 0;
+    for (var i = 0; i < $scope.cart.length; i++) {
+      quantity += $scope.cart[i].count;
+    };
+    return quantity;
+  };
+
+  $scope.clearCart = function() {
+    $scope.cart = [];
+  }
+
+  $scope.search = function() {
+    console.log("key pressed");
   };
 };
