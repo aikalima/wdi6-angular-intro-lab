aa32987f913f9a940a0d511701d0d228cce90e2c
SylvieC <sylviecottrell@gmail.com>
2014-03-11 01:06:30 -0700

    fixed mistake concerning quantity

--- app/books_controller.js
+++ app/books_controller.js
@@ -13,29 +13,18 @@ bookly.BooksController = function($scope) {
   // $scope.cart_to_display = _.uniq($scope.cart);
 
 
-  // I create an array called cart_qty of length (books.length + 1), each element at index i is the number of books
-  // with id i. Each value is 0.
-  var cart_qty = [];
-
-  for (var i = 1; i <= books.length; i++) {
-    cart_qty[i] = 0;
-  }
-
-
   // code without quantity column
   $scope.add = function(book) {
     $scope.cart.push(book);
-
-
     $scope.cart = _.uniq($scope.cart);
-    if (($scope.cart.length === 1) || (in_cart(book) === false)) {
+    if (($scope.cart.length === 1) || ($scope.in_cart(book) === false)) {
       $scope.total += book.price;
     }
-    $scope.cart_qty[book.id] += 1;
+    // $scope.cart_qty[book.id] += 1;
   };
 
 
-  $scope.count = function count(book) {
+  $scope.count = function(book) {
     var num = 0;
     for (var i = 0; i < $scope.cart.length; i++) {
       if (_.isEqual($scope.cart[i], book)) {
@@ -45,7 +34,7 @@ bookly.BooksController = function($scope) {
     return num;
   };
 
-  var in_cart = function(book) {
+  $scope.in_cart = function(book) {
     for (var ele in books) {
       if (_.isEqual(ele, book)) {
         return true;
@@ -54,13 +43,14 @@ bookly.BooksController = function($scope) {
     }
   };
 
-  $scope.calculate_total = function() {
-    var a = 0;
-    for (var i = 0; i < $scope.cart.length; i++) {
-      a += (array[i].price);
-    }
-    return a;
-  };
+  // $scope.calculate_total = function(array) {
+  //   var a = 0;
+  //   for (var i = 0; i < $scope.cart.length; i++) {
+  //     var array = $scope.cart;
+  //     a += (array[i].price);
+  //   }
+  //   return a;
+  // };
 
 
   $scope.remove = function() {
